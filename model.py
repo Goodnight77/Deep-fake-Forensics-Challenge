fd1eb810052b51f4a532fac94da5d27fdc6ac036
Goodnight <117648012+Goodnight77@users.noreply.github.com>
2023-12-11 22:28:13 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications import ResNet50
